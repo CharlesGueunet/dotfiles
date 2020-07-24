44ce6f619134f46a740abfbf60ad7e403464870b
Charles Gueunet <charles.gueunet@gmail.com>
2020-07-24 09:04:27 +0200

    separate parts in the bar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,9 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar main >>/tmp/polybar1.log 2>&1 &
+polybar left >>/tmp/polybar1.log 2>&1 &
+polybar center >>/tmp/polybar2.log 2>&1 &
+polybar right >>/tmp/polybar3.log 2>&1 &
 
 
 echo "Bars launched..."
