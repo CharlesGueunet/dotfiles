b45bd0e3402acd99f364fbcc97ab52bfba2bab0e
Charles Gueunet <charles.gueunet@gmail.com>
2020-07-27 08:20:17 +0200

    Single bar is better

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,9 +7,10 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar left >>/tmp/polybar1.log 2>&1 &
-polybar center >>/tmp/polybar2.log 2>&1 &
-polybar right >>/tmp/polybar3.log 2>&1 &
+polybar main >>/tmp/polybar1.log 2>&1 &
 
+# polybar left >>/tmp/polybar1.log 2>&1 &
+# polybar center >>/tmp/polybar2.log 2>&1 &
+# polybar right >>/tmp/polybar3.log 2>&1 &
 
 echo "Bars launched..."
